9499f1aa8d50d7819b355b11ff7cdbb5c6d930e8
firstChairCoder <md1qq7@gmail.com>
2024-04-29 17:11:37 +0100

    chore: complete endpoint whoami

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+let resObject = {};
+app.enable('trust proxy'); // allows for access to origin client IP address
+// endpoint that returns required request properties
+app.get("/api/whoami", function (req, res) {
+resObject["ipaddress"] = req.ip;
+resObject["language"] = req.get("Accept-Language");
+resObject["software"] = req.get("User-Agent");
+
+res.json(resObject)
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
